d7a22448d0b192ccc76ab9437ab2a304ea450cc8
Jonas <paandab321@gmail.com>
2020-04-17 14:31:22 +0200

    Tried to enemy

---
+++ Assets/Scripts/Jonas/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public EnemyScript myParent;
+
+    void Start()
+    {
+        myParent = GetComponentInParent<EnemyScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, myParent.shootPoint, myParent.bulletForce);
+    }
+}
---
+++ Assets/Scripts/Jonas/EnemyHitBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitBox : MonoBehaviour
+{
+    public bool playerInRange;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
