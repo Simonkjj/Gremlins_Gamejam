9af75efe36bd3a3ecfb40b4f5ea97c603a6cbda8
Jonathan August Michelsen <jonathan.99@live.no>
2020-04-17 12:31:06 +0200

    Test Push, Working on JamTestScene

---
+++ Assets/Scripts/Test Scripts/testClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testClick : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int RayDistance = 10;
+    private Vector3 Point;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Test Scripts/testMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMove : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+}
