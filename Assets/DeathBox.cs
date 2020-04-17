755f25b186fdf048f3eb470a7b115ab670f84921
Simon Jacobsen <simon.jac@hotmail.com>
2020-04-17 14:15:29 +0200

    DEATH BOX ADDED

---
+++ Assets/DeathBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    public DeathScript ds;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == ("Player"))
+        {
+            ds.IsDead();
+        }
+    }
+}
