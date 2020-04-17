8d9eb6600d19e8a3cfd1ba1c16ebf4acbca8ad80
Jonas <paandab321@gmail.com>
2020-04-17 14:04:04 +0200

    Grapple

---
+++ Assets/Scripts/Jonas/GrabPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabPoint : MonoBehaviour
+{
+    public LineRenderer myLine;
+
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        myLine.GetComponent<LineRenderer>();
+        myLine.SetPosition(0, player.transform.position);
+        myLine.SetPosition(1, MousePos.mousePos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLine.SetPosition(0, player.transform.position);
+    }
+}
---
+++ Assets/Scripts/Jonas/MousePos.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePos : MonoBehaviour
+{
+    public Grapple myGrap;
+    public static Vector2 mousePos;
+
+    void Start()
+    {
+        myGrap = GetComponentInParent<Grapple>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMousePos();
+        MoveMeAround();
+    }
+
+    public void GetMousePos()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void MoveMeAround()
+    {
+        transform.position = mousePos;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            myGrap.mouseOverSomething = true;
+        }
+        else
+        {
+            myGrap.mouseOverSomething = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        myGrap.mouseOverSomething = false;
+    }
+}
