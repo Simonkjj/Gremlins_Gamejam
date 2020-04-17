75653f592549897f1cfbcb1f83aa0f5a69c8e218
Simon Jacobsen <simon.jac@hotmail.com>
2020-04-17 12:33:05 +0200

    Added button bar

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -16,57 +16,51 @@ public class PlayerScript : MonoBehaviour
 
     [SerializeField]
     private bool isRunning = false;
+    [SerializeField]
     private bool isGrounded = false;
     private bool canSlide = false;
 
 
-
     private Rigidbody2D rb;
 
-    //private Button slideButton;
-    //private Button jumpButton;
-    //private Button runButton;
-    //private Button stopButton;
+    [Header("Buttons")]
+    public Button jumpButton;
+    public Button runButton;
+    public Button stopButton;
+    public Button turnButton;
 
-    [Header("Public gameobjects")]
-    public HealthSystem healthScr;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //runButton = GameObject.FindGameObjectWithTag("RunButton").GetComponent<Button>();
-        //slideButton = GameObject.FindGameObjectWithTag("SlideButton").GetComponent<Button>();
-        //jumpButton = GameObject.FindGameObjectWithTag("JumpButton").GetComponent<Button>();
-        //stopButton = GameObject.FindGameObjectWithTag("StopButton").GetComponent<Button>();
-
-
 	}
 
-    void FixedUpdate()
+    void Update()
     {
         if (isRunning)
         {
-            canSlide = true;
-            //jumpButton.interactable = true;
-            //slideButton.interactable = true;
-            //stopButton.interactable = true;
-            //runButton.interactable = false;
-        } 
-		else
+            jumpButton.interactable = true;
+            stopButton.interactable = true;
+            runButton.interactable = false;
+            turnButton.interactable = true;
+        }
+        else
         {
-            //jumpButton.interactable = false;
-            //slideButton.interactable = false;
-            //stopButton.interactable = false;
-            //runButton.interactable = true;
+            jumpButton.interactable = false;
+            stopButton.interactable = false;
+            runButton.interactable = true;
+            turnButton.interactable = true;
         }
 
         if (isGrounded)
         {
-            //jumpButton.interactable = true;
+            jumpButton.interactable = true;
+            turnButton.interactable = true;
         }
         else
         {
-            //jumpButton.interactable = false;
-            //runButton.interactable = false;
+            jumpButton.interactable = false;
+            runButton.interactable = false;
+            turnButton.interactable = false;
         }
 
     }
@@ -78,20 +72,18 @@ public class PlayerScript : MonoBehaviour
             isGrounded = true;
         }
 
-        if (col.gameObject.tag == "DamageObject")
-        {
-            healthScr.IsHit();
-            SlideButton();
-
-            Debug.Log("Lose health and slide");
-        }
+        //if (col.gameObject.tag == "DamageObject")
+        //{
+        //    healthScr.IsHit();
+        //    Debug.Log("Lose health and slide");
+        //}
     }
 
     public void Run()
     {
         if (isGrounded)
 		{
-			rb.velocity = new Vector2(speed * 0.02f, rb.velocity.y);
+			rb.velocity = new Vector2(speed, rb.velocity.y);
 			isRunning = true;
         }
     }
@@ -105,41 +97,19 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    public void SlideButton()
-    {
-        StartCoroutine(Slide());
-    }
-
-    public IEnumerator Slide()
-    {
-        if (isRunning)
-        {
-            canSlide = false;
-            float defaultScale = transform.localScale.y;
-            while (transform.localScale.y > defaultScale / 2)
-            {
-                transform.localScale -= new Vector3(0, scaleValue, 0);
-                yield return new WaitForSeconds(shrinkTime);
-            }
-
-            yield return new WaitForSeconds(slideTime);
-
-            while (transform.localScale.y < defaultScale)
-            {
-                transform.localScale += new Vector3(0, scaleValue, 0);
-                yield return new WaitForSeconds(shrinkTime);
-            }
-            canSlide = true;
-            Run();
-            yield return null;
-        }
-        yield return null;
-    }
-
     public void Stop()
     {
         isRunning = false;
         rb.velocity = new Vector2(0, rb.velocity.y);
+    }
+
+    public void Turn()
+    {
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            isRunning = true;
+        }
     }
 }
 
