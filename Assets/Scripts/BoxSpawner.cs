7feea22f4b060dc078350f3b0d541629f6d09100
Jonathan August Michelsen <jonathan.99@live.no>
2020-04-17 13:42:28 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject Box;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0.0f;
+            GameObject objectInstance = Instantiate(Box, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+        }
+    }
+}
--- Assets/Scripts/Test Scripts/testClick.cs
+++ Assets/Scripts/Test Scripts/testClick.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testClick : MonoBehaviour
 {
     public GameObject Prefab;
-    public int RayDistance = 10;
-    private Vector3 Point;
+    public bool isClicked;
 
-    public void Update()
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0))
+        isClicked = false;
+    }
+
+    void Clicked()
+    {
+        isClicked = true;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && (isClicked == true))
         {
-            
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0.0f;
+            GameObject objectInstance = Instantiate(Prefab, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
         }
     }
 }
